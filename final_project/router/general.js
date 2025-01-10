1ff7b70c7fe083c8f1ab0f3937043f3a18f6e8cf
tomoldmeadow <tomoldmeadow@icloud.com>
2025-01-10 12:25:48 -0500

    Tasks 1 and 2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,20 +12,18 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify({books},null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const ISBN = req.params.isbn;
+  res.send(books[ISBN])
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    // Obtain all keys for books object
 });
 
 // Get all books based on title
